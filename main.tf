e1097a5dc18be7b08fc3c901c1d550f226a33bd4
Guy Radford <guy.radford@sunswap.co.uk>
2023-03-30 14:58:18 +0100

    initial

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.47"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "name" {
+  type = string
+}
+
+locals {
+  environment = terraform.workspace
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "rg-${ var.name }-${ local.environment }"
+  location = "uksouth"
+}
